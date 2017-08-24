9d2a6b225cb8f6716d13a0a3089268d29f0a34c5
Shannon Nichols <shannonsn19@gmail.com>
2017-08-24 15:11:17 +0200

    edit the readme file

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ app.use(bodyParser.json());
    });
  });
 
- app.get('/', function(req, res){res.redirect('/reg_numbers')
+ // app.get('/', function(req, res){res.redirect('/reg_numbers')})
  app.post('/reg_numbers', registrations.regPlateNumberFunction);
-})
+
 
 
   const port = process.env.PORT || 3005;
